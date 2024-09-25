787e05b81394713408b710467c0834c29a86bc45
Sonhador Brasil <magazinegravatai@gmail.com>
2024-09-24 23:40:21 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,7 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 
-export default function Home() {
-    const [data, setData] = useState([]);
+const Home = () => {
+  return <div>Olá, este é o meu projeto Next.js!</div>;
+};
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch('/api/scrape');
-            const result = await response.json();
-            setData(result);
-        };
-
-        fetchData();
-    }, []);
-
-    return (
-        <div>
-            <h1>Resultados do Scraping</h1>
-            <ul>
-                {data.map((item, index) => (
-                    <li key={index}>
-                        <strong>Resultado:</strong> {item.result} | <strong>Data:</strong> {item.date}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+export default Home;
